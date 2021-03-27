c51aafd97517e36c44b3d4ed7ff6e9b51e095c05
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2021-03-27 18:29:50 +0200

    Adds application components
    * adds the thread list component
    
    * adds the page not found component

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,51 +1,19 @@
 <template>
-  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
-    <h1>{{ thread.title }}</h1>
-
-    <div class="post-list">
-
-      <div v-for="postId in thread.posts" :key="postId" class="post">
-        <div class="user-info">
-          <a href="#" class="user-name">{{ userById(postById(postId).userId).name }}</a>
-          <a href="#"><img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt=""></a>
-          <p class="desktop-only text-small">107 posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>{{ postById(postId).text }}</p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">{{ postById(postId).publishedAt }}</div>
-
-      </div>
-
-    </div>
-
-  </div>
+  <h1>Welcome to the forum</h1>
+  <thread-list :threads="threads"/>
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import ThreadList from './ThreadList'
 
 export default {
   name: 'PageHome',
+  components: { ThreadList },
   data () {
     return {
-      threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users
+      threads: sourceData.threads
     }
-  },
-  methods: {
-    postById (postId) {
-      return this.posts.find(p => p.id === postId)
-    },
-    userById (userId) {
-      return this.users.find(u => u.id === userId)
-    }
-
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import PageHome from '@/components/PageHome'
 import PageThreadShow from '@/components/PageThreadShow'
+import PageNotFound from '@/components/PageNotFound'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', name: 'Home', component: PageHome },
-  { path: '/thread/:id', name: 'ThreadShow', component: PageThreadShow, props: true }
+  { path: '/thread/:id', name: 'ThreadShow', component: PageThreadShow, props: true },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: PageNotFound }
 ]
 
 export default createRouter({
