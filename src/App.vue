49996ee291fdfb89b97268083795c688ea768bc9
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2021-03-27 17:12:08 +0200

    PageThreadShow
    * adds the page tread show
    
    * adds the router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <div class="container">
-    <page-home></page-home>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import PageHome from './components/PageHome'
-
 export default {
-  name: 'App',
-  components: {
-    PageHome
-  }
+  name: 'App'
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PageHome from './components/PageHome'
+import PageThreadShow from './components/PageThreadShow'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', name: 'Home', component: PageHome },
+  { path: '/thread/:id', name: 'ThreadShow', component: PageThreadShow, props: true }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const forumApp = createApp(App)
+forumApp.use(router)
+forumApp.mount('#app')
