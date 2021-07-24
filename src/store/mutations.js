2a5878e62dc7fa5a3ffb218a59ee100c484da176
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2021-07-24 15:54:09 +0300

    Splitting the vuex store into namespaced modules

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,39 +1,13 @@
-import { findById, upsert, docToResource } from '@/helpers'
+import { upsert, docToResource } from '@/helpers'
 
 export default {
   setItem (state, { resource, item }) {
     upsert(state[resource], docToResource(item))
-  },
-  setAuthId (state, id) {
-    state.authId = id
-  },
-  setAuthUserUnsubscribe (state, unsubscribe) {
-    state.authUserUnsubscribe = unsubscribe
-  },
-  setAuthObserverUnsubscribe (state, unsubscribe) {
-    state.authObserverUnsubscribe = unsubscribe
   },
   appendUnsubscribe (state, { unsubscribe }) {
     state.unsubscribes.push(unsubscribe)
   },
   clearAllUnsubscribes (state) {
     state.unsubscribes = []
-  },
-  appendPostToThread: makeAppendChildToParentMutation({ parent: 'threads', child: 'posts' }),
-  appendThreadToForum: makeAppendChildToParentMutation({ parent: 'forums', child: 'threads' }),
-  appendThreadToUser: makeAppendChildToParentMutation({ parent: 'users', child: 'threads' }),
-  appendContributorToThread: makeAppendChildToParentMutation({ parent: 'threads', child: 'contributors' })
-}
-
-function makeAppendChildToParentMutation ({ parent, child }) {
-  return (state, { childId, parentId }) => {
-    const resource = findById(state[parent], parentId)
-    if (!resource) {
-      return
-    }
-    resource[child] = resource[child] || []
-    if (!resource[child].includes(childId)) {
-      resource[child].push(childId)
-    }
   }
 }
