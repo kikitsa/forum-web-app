2d8fc30b639a077b90ea144cf3b2ecf61fc657ff
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2021-07-24 16:53:10 +0300

    Vuex store: Namespaced modules

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,14 +1,15 @@
 import firebase from 'firebase'
 
 export default {
+  namespaced: true,
   state: {
     authId: null,
     authUserUnsubscribe: null,
     authObserverUnsubscribe: null
   },
   getters: {
-    authUser: (state, getters) => {
-      return getters.user(state.authId)
+    authUser: (state, getters, rootState, rootGetters) => {
+      return rootGetters['users/user'](state.authId)
     }
   },
   actions: {
@@ -30,7 +31,7 @@ export default {
     },
     async registerUserWithEmailAndPassword ({ dispatch }, { avatar = null, email, name, username, password }) {
       const result = await firebase.auth().createUserWithEmailAndPassword(email, password)
-      await dispatch('createUser', { id: result.user.uid, email, name, username, avatar })
+      await dispatch('users/createUser', { id: result.user.uid, email, name, username, avatar }, { root: true })
     },
     signInWithEmailAndPassword (context, { email, password }) {
       return firebase.auth().signInWithEmailAndPassword(email, password)
@@ -42,7 +43,10 @@ export default {
       const userRef = firebase.firestore().collection('users').doc(user.uid)
       const userDoc = await userRef.get()
       if (!userDoc.exists) {
-        return dispatch('createUser', { id: user.uid, name: user.displayName, email: user.email, username: user.email, avatar: user.photoURL })
+        return dispatch('users/createUser',
+          { id: user.uid, name: user.displayName, email: user.email, username: user.email, avatar: user.photoURL },
+          { root: true }
+        )
       }
     },
     async signOut ({ commit }) {
@@ -60,13 +64,15 @@ export default {
         handleUnsubscribe: (unsubscribe) => {
           commit('setAuthUserUnsubscribe', unsubscribe)
         }
-      })
+      },
+      { root: true }
+      )
       commit('setAuthId', userId)
     },
     async fetchAuthUsersPosts ({ commit, state }) {
       const posts = await firebase.firestore().collection('posts').where('userId', '==', state.authId).get()
       posts.forEach(item => {
-        commit('setItem', { resource: 'posts', item })
+        commit('setItem', { resource: 'posts', item }, { root: true })
       })
     },
     async unsubscribeAuthUserSnapshot ({ state, commit }) {
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,7 +2,7 @@ import { upsert, docToResource } from '@/helpers'
 
 export default {
   setItem (state, { resource, item }) {
-    upsert(state[resource], docToResource(item))
+    upsert(state[resource].items, docToResource(item))
   },
   appendUnsubscribe (state, { unsubscribe }) {
     state.unsubscribes.push(unsubscribe)
