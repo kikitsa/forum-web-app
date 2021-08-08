7af623a1d6cc93d461cbd0997c324f765c535beb
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2021-08-08 17:13:57 +0300

    Storing the Current Page as a URL Query Variable

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <the-nav-bar></the-nav-bar>
   <div class="container">
-    <router-view v-show="showPage" @ready="onPageReady" :key="$route.path"/>
+    <router-view v-show="showPage" @ready="onPageReady" :key="`${$route.path}${JSON.stringify($route.query)}`"/>
     <app-spinner v-show="!showPage" />
   </div>
 </template>
