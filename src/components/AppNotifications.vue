b8867b0cb9fea73f36d6d49183f28e13c783ff37
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2021-10-02 11:15:26 +0300

    Notifications: Using the transition component

--- src/components/AppNotifications.vue
+++ src/components/AppNotifications.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="notifications">
-    <div
-      class="notification"
-      v-for="notification in notifications"
-      :key="notification.id"
-    >
-      <span>{{ notification.message }}</span>
-      <button @click="removeNotification(notification.id)">x</button>
-    </div>
+    <transition-group name="notification">
+      <div
+        class="notification"
+        v-for="notification in notifications"
+        :key="notification.id"
+      >
+        <span>{{ notification.message }}</span>
+        <button @click="removeNotification(notification.id)">x</button>
+      </div>
+    </transition-group>
   </div>
 </template>
 
@@ -37,5 +39,17 @@ export default {
   padding: 10px 20px;
   margin-bottom: 5px;
   border-left: 5px solid #263959;
+}
+.notification-enter-active,
+.notification-leave-active {
+  transition: all .5s ease;
+}
+.notification-enter-from,
+.notification-leave-to {
+  opacity: 0;
+  transform: translateX(100%);
+}
+.notification-move {
+  transition: transform 0.8s ease;
 }
 </style>
