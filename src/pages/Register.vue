d823749cc897007f9df74a15f110bd3a01ac1bf1
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2021-10-28 14:18:38 +0300

    Adding global rules for VeeValidate

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -6,12 +6,12 @@
 
         <div class="form-group">
           <label for="name">Full Name</label>
-          <VeeField name="name" v-model="form.name" id="name" type="text" class="form-input" :rules="required" />
+          <VeeField name="name" v-model="form.name" id="name" type="text" class="form-input" rules="required" />
           <VeeErrorMessage name="name" class="form-error" />
         </div>
         <div class="form-group">
           <label for="username">Username</label>
-          <VeeField name="username" v-model="form.username" id="username" type="text" class="form-input" :rules="required" />
+          <VeeField name="username" v-model="form.username" id="username" type="text" class="form-input" rules="required" />
           <VeeErrorMessage name="username" class="form-error" />
         </div>
 
@@ -57,14 +57,7 @@
 </template>
 
 <script>
-import { Form, Field, ErrorMessage } from 'vee-validate'
-
 export default {
-  components: {
-    VeeForm: Form,
-    VeeField: Field,
-    VeeErrorMessage: ErrorMessage
-  },
   data () {
     return {
       avatarPreview: null,
@@ -78,10 +71,6 @@ export default {
     }
   },
   methods: {
-    required (value) {
-      if (value && value.trim()) return true
-      return 'This is required'
-    },
     async register () {
       await this.$store.dispatch('auth/registerUserWithEmailAndPassword', this.form)
       this.$router.push('/')
