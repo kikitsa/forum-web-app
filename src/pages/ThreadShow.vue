f603e34169ec40af438fa425bf82534bd7533cb3
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2021-10-28 16:29:59 +0300

    Clean up

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -12,7 +12,10 @@
       By <a href="#" class="link-unstyled">{{ thread.author?.name }}</a>,
       <app-date :timestamp="thread.publishedAt"/>.
       <span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">
-        {{ thread.repliesCount }} replies by {{ thread.contributorsCount}} contributors
+        {{thread.repliesCount}}
+        {{thread.repliesCount === 1 ? 'reply' : 'replies'}}
+        by {{thread.contributorsCount}}
+        {{thread.contributorsCount === 1 ? 'contributor' : 'contributors'}}
       </span>
     </p>
 
